4cc5cfbb8d9e524ccb0725e30067dbf3cafa4285
loves <loves@YUMI>
2023-05-02 16:59:22 +0900

    문제풀이

---
+++ BackJoon/src/q1008/Main.java
@@ -0,0 +1,13 @@
+package q1008;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+
+	}
+
+}
---
+++ BackJoon/src/q9498/Main.java
@@ -0,0 +1,23 @@
+package q9498;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		sc.close();
+		if ((100 >= a && 90 <= a)) {
+			System.out.println("A");
+		} else if ((89 >= a && 80 <= a)) {
+			System.out.println("B");
+		} else if ((79 >= a && 70 <= a)) {
+			System.out.println("C");
+		} else if ((69 >= a && 60 <= a)) {
+			System.out.println("D");
+		} else if (0 <= a) {
+			System.out.println("F");
+		}
+	}
+}
