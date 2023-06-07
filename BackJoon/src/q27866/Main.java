78e98d7dd25d36f327b5ba7cbc26df4cf8fd6d81
loves <loves@YUMI>
2023-06-07 14:35:53 +0900

    문제풀이

---
+++ BackJoon/src/q27866/Main.java
@@ -0,0 +1,15 @@
+package q27866;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String S = sc.next();
+		int i = sc.nextInt();
+		sc.close();
+		System.out.println(S.charAt(i - 1));
+
+	}
+}
